a26661f66c75ef8b6a0a327205907dd0a2506f38
wiliamfeng <wiliamfeng@gmail.com>
2022-01-05 06:35:13 +0700

    config axios

--- client/src/configs/axios-client.ts
+++ client/src/configs/axios-client.ts
@@ -2,6 +2,7 @@ import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { getTokenLocalStorage, setTokenLocalStorage } from 'src/utils';
 import dayjs from 'dayjs';
 import jwt_decode from 'jwt-decode';
+import { ACCESS_TOKEN_KEY } from 'src/configs/const';
 
 const baseURL = process.env.REACT_APP_BACKEND_URL;
 
@@ -14,78 +15,64 @@ const axiosClient = axios.create({
 });
 
 axiosClient.interceptors.request.use(async (request: AxiosRequestConfig) => {
-  const accessToken = getTokenLocalStorage('access_token');
+  const accessToken = getTokenLocalStorage(ACCESS_TOKEN_KEY);
   if (accessToken) {
-    request.headers.Authorization = `Bearer ${JSON.parse(accessToken)}`;
+    const user = jwt_decode(accessToken);
+    const isExpired = dayjs.unix(user?.exp).diff(dayjs()) < 1;
+    if (!isExpired) {
+      request.headers.Authorization = `Bearer ${JSON.parse(accessToken)}`;
+      return request;
+    }
   }
 
-  const user = jwt_decode(accessToken);
-  const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1;
-
-  if (!isExpired) return request;
-
   const response = await axios.post(`${baseURL}/auth/refresh-token`);
-
   const { access_token } = response.data;
-
-  setTokenLocalStorage('access_token', access_token);
-
+  setTokenLocalStorage(ACCESS_TOKEN_KEY, access_token);
   request.headers.Authorization = `Bearer ${access_token}`;
   return request;
 });
 
 axiosClient.interceptors.response.use(
   (response: AxiosResponse) => {
-    console.log('successs - response >>>', response);
     return response.data;
     // return new Promise((resolve, reject) => {
     //   resolve(response);
     // });
   },
   (error) => {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-
-      const { status, data } = error.response;
-      const originalRequest = error.config;
-
-      // that falls out of the range of 2xx
-      console.log('case-1 - data >>>>>>>>', data);
-      console.log('case-1 - status >>>>>>>>', status);
-
-      // wrong end-point:  request with endpoint is not defined on server
-      if (status === 404) {
-        throw Error('The request is not valid');
-      }
-
-      // refresh-token: expired
-      if (status === 401 && data.message === 'Invalid refresh token') {
-        localStorage.removeItem('access_token');
-        window.location.href = '/auth/login';
-        throw Error('Unauthorize please login');
-      }
-
-      // forbidden
-      if (error.response.status === 403) {
-        window.location.href = '/auth/login';
-        throw Error('Unauthorize please login');
-      }
-
-      throw Error(error.response.data.message);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-      // http.ClientRequest in node.js
-      // Khong co ket noi mang
-      // Server dang tat khong ket noi den duoc
-      console.error('case-2 - error >>>>>>>>', error);
-      console.log('case-2 - error.request >>>>>>>>', error.request);
-
-      throw Error('Could not fetch the data for that resource');
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log('case-3 >>>>>>>>', error.message);
+    if (error.message === 'Network Error' && !error.response) {
+      // toast.error('Network error - make sure API is running!')
     }
+
+    // The request was made and the server responded with a status code
+    const { status, data, config } = error.response;
+
+    if (status === 404) {
+      // not found
+      throw Error('The request is not valid');
+    }
+
+    if (status === 400 && config.method === 'get' && data.errors.hasOwnProperty('id')) {
+      // wrong end-point:  request with endpoint is not defined on server
+    }
+
+    if (status === 500) {
+      // toast.error('Server error -check the terminal for more info')
+    }
+
+    if (status === 401 && data.message === 'Invalid refresh token') {
+      // refresh-token: expired
+      localStorage.removeItem('access_token');
+      window.location.href = '/auth/login';
+      throw Error('Unauthorize please login');
+    }
+
+    if (error.response.status === 403) {
+      // forbidden
+      window.location.href = '/auth/login';
+      throw Error('Unauthorize please login');
+    }
+
     return Promise.reject(error);
   }
 );
